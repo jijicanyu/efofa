26a8300499205328efc255aa95be75ff93b3f619
LubyRuffy <lubyruffy@gmail.com>
2015-07-28 01:58:01 +0800

    修改redis连接池延时

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,11 +8,11 @@ config = YAML.load_file(rails_root + '/config/database.yml')[rails_env]['redis']
 redis_url = "redis://#{config['host']}:#{config['port']}/#{config['db']}"
 
 Sidekiq.configure_server do |cfg|
-  cfg.redis = { :url => redis_url, :namespace => "#{config['namespace']}", :size => 2 }
+  cfg.redis = { :url => redis_url, :namespace => "#{config['namespace']}", :size => 2, :pool_timeout => 5}
 end
 
 Sidekiq.configure_client do |cfg|
-  cfg.redis = { :url => redis_url, :namespace => "#{config['namespace']}", :size => 2 }
+  cfg.redis = { :url => redis_url, :namespace => "#{config['namespace']}", :size => 2, :pool_timeout => 5 }
 end
 
 
