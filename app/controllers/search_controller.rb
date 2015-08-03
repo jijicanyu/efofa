d425ea8592380cd871ac1157d6ea9655987d0a24
LubyRuffy <lubyruffy@gmail.com>
2015-08-03 16:40:36 +0800

    首页倒序显示

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,20 @@
 class SearchController < ApplicationController
   def index
+    per_page = 10
+    per_page = params[:per_page].to_i if params[:per_page]
+    page = 1
+    page = params[:page].to_i if params[:page]
+
+    @hosts = Subdomain.all(
+        query: { match_all: {} },
+        _source: ['host', 'title', 'lastupdatetime'],
+        sort:[
+            {
+                lastupdatetime: "desc"
+            }
+        ],
+        size: per_page,
+        from: (page-1)*10)
     @count = Subdomain.es_length
   end
 
